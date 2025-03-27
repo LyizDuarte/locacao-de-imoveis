16af60dc56466abaa13a754f7c02dc3c5cad29b4
Luiz Duarte <looduarte21@gmail.com>
2025-03-26 21:55:16 -0300

    feat: token novo ao expirar

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken"
 import UsuarioRepository from "../repositories/usuarioRepository.js"
-
 const SECRET = "C0R1NTH14N5"
 
 export default class AuthMiddleware {
@@ -13,31 +12,57 @@ export default class AuthMiddleware {
         perfilId: perfilId,
       },
       SECRET,
-      { expiresIn: 3600 }
+      { expiresIn: 5 }
     )
   }
 
   async validar(req, res, next) {
+    let that = this
     if (req.headers.authorization == null)
-      res.status(401).json({ msg: "Token de acesso não foi enviado" })
+      return res.status(401).json({ msg: "Token de acesso não foi enviado!" })
 
     let token = req.headers["authorization"].split(" ")[1]
     if (token) {
-      let usuarioToken = jwt.verify(token, SECRET)
-      let repo = new UsuarioRepository()
-      let usuarioBanco = await repo.obter(usuarioToken.id)
-      if (usuarioBanco.length > 0) {
-        if (usuarioBanco[0].ativo) {
-          req.usuarioLogado = usuarioBanco[0]
-          next()
+      let usuarioToken = null
+      let tokenValido = false
+      try {
+        usuarioToken = jwt.verify(token, SECRET)
+        tokenValido = true
+      } catch (ex) {
+        if (ex.name == "TokenExpiredError") {
+          usuarioToken = jwt.verify(token, SECRET, { ignoreExpiration: true })
+          let auth = new AuthMiddleware()
+          let novoToken = auth.gerarToken(
+            usuarioToken.id,
+            usuarioToken.nome,
+            usuarioToken.email,
+            usuarioToken.perfilId
+          )
+
+          res.set["authorization"] = `Bearer ${novoToken}`
+
+          tokenValido = true
+        } else res.status(401).json({ msg: "Não autorizado!" })
+      }
+      if (tokenValido) {
+        //valida no banco;
+        let repo = new UsuarioRepository()
+        let usuarioBanco = await repo.obter(usuarioToken.id)
+        if (usuarioBanco.length > 0) {
+          if (usuarioBanco[0].ativo) {
+            req.usuarioLogado = usuarioBanco[0]
+            next()
+          } else {
+            res.status(401).json({ msg: "Usuário inativo" })
+          }
         } else {
-          res.status(401).json({ msg: "Usuário inativo" })
+          res.status(401).json({ msg: "Não autorizado!" })
         }
       } else {
         res.status(401).json({ msg: "Não autorizado!" })
       }
     } else {
-      return res.status(401).json({ msg: "Não autorizado!" })
+      res.status(401).json({ msg: "Não autorizado!" })
     }
   }
 }
